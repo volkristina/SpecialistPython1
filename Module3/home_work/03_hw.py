fac1774799a86708c1473902ff8db8df213d5df5
volkristina <106874076+volkristina@users.noreply.github.com>
2022-06-27 22:52:22 +0300

    Update 03_hw.py
    # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -100 до 100.
    # Вывести на экран сумму всех положительных элементов кратных двум.
    import random
    numbers = []
    n = 8
    for _ in range(n):
        number = random.randint(-100, 100)
        numbers.append(number)
    print(numbers)
    summa = 0
    for number in numbers:
        if number % 2 == 0 and number > 0:
            summa += number
    
    print(summa)

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,18 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+# Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -100 до 100.
+# Вывести на экран сумму всех положительных элементов кратных двум.
+import random
+numbers = []
+n = 8
+for _ in range(n):
+    number = random.randint(-100, 100)
+    numbers.append(number)
+print(numbers)
+summa = 0
+for number in numbers:
+    if number % 2 == 0 and number > 0:
+        summa += number
+
+print(summa)
